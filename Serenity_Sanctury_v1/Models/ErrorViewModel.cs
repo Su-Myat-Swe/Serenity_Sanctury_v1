7e076169c39392ecba4453f389a7a21bc11d3d8a
Su-Myat-Swe <sumyatswe16@gmail.com>
2024-05-26 01:27:11 +0800

    Add project files.

---
+++ Serenity_Sanctury_v1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Serenity_Sanctury_v1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

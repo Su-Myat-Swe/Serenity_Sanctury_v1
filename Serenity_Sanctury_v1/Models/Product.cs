05755a49ba383332491aa627e4d4251855377a45
Su-Myat-Swe <sumyatswe16@gmail.com>
2024-06-27 20:51:20 +0800

    Added Product table

---
+++ Serenity_Sanctury_v1/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
+namespace Serenity_Sanctury_v1.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public char Category { get; set; }
+        public string Description { get; set; }
+        public int Color { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}

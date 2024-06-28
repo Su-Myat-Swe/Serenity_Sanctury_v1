6291e57fce67ac07f25f78eb0e0947b5b8c53d0b
Su-Myat-Swe <sumyatswe16@gmail.com>
2024-06-28 10:47:15 +0800

    Added Order table

---
+++ Serenity_Sanctury_v1/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿namespace Serenity_Sanctury_v1.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        public string ShippingAddress { get; set; }
+        public string OrderDate { get; set; }
+        public decimal TotalPrice { get; set; }
+        public OrderStatus Status { get; set; }
+
+        // Define the Status enum within the Order class or separately
+        public enum OrderStatus
+        {
+            Pending,
+            Shipped,
+            Delivered,
+            Cancelled
+        }
+    }
+}

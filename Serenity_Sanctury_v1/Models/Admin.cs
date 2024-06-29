471f4d230d2d0a1ff4ec6dfac36704f2f1faf4ae
Su-Myat-Swe <sumyatswe16@gmail.com>
2024-06-29 17:47:57 +0800

    saved code and made changes in classes

--- Serenity_Sanctury_v1/Models/Admin.cs
+++ Serenity_Sanctury_v1/Models/Admin.cs
@@ -1,7 +1,11 @@
-﻿namespace Serenity_Sanctury_v1.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Serenity_Sanctury_v1.Models
 {
     public class Admin
     {
+        [Key]
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
@@ -9,6 +13,7 @@
         public string PhoneNo { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string Address { get; set; }
+        // Navigation property
+        public Address? Address { get; set; }
     }
 }
